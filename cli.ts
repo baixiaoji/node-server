adc775ba06cb166a79d6de8f2643b1432b4967e8
baixiaoji <1041874421@qq.com>
2019-11-06 11:22:18 +0800

    :fire: 添加了终端的命令

---
+++ cli.ts
@@ -0,0 +1,19 @@
+const program = require('commander');
+const pkg = require('./package.json');
+const {kickOff} = require('./index');
+program.version(pkg.version);
+
+
+program
+    .option('-p, --port <portNumber>', 'listen port number')
+    .option('-c, --cache [cacheTime]', 'cache time [default to 10days]')
+    .option('-d, --dirName [directory]', 'server in which directory [default to current dir]');
+
+program.parse(process.argv);
+
+
+export interface IArgs {
+    [k: string]: string
+}
+
+kickOff(program.opts());
--- index.ts
+++ index.ts
@@ -5,44 +5,48 @@ import * as fs from "fs";
 import * as p from 'path';
 import * as url from "url";
 import * as mime from 'mime';
+import {IArgs} from "./cli";
 
-const server = http.createServer();
-const publicPath = p.resolve(__dirname, './public');
-const cacheTime = 3600 * 24 * 30;
-server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-    const { method, url:path } = request;
-    const {pathname} = url.parse(path);
-    if (method !== 'GET') {
-        response.statusCode = 405;
-        response.end('服务器繁忙');
-        return;
-    }
-    let fileName = pathname.slice(1);
-    if (fileName === '') {
-        fileName = 'index.html'
-    }
-    fs.readFile(p.resolve(publicPath,  fileName), (err, data) => {
-        const fileType = mime.getType(fileName.split('.')[1]);
-        response.setHeader('Content-Type', fileType);
-        response.setHeader('Cache-Control', cacheTime);
-        if (err) {
-            if (err.errno === -2) {
-                response.statusCode = 404;
-                fs.readFile(p.resolve(publicPath, '404.html'), (err, data) => {
-                    response.end(data);
-                })
-            } else {
-                response.statusCode = 500;
-                response.end('服务器正忙');
-            }
+
+module.exports.kickOff = ({port = '8080', cache, dir = '.'}:IArgs) => {
+    const server = http.createServer();
+    const publicPath = p.resolve(__dirname, dir);
+    const cacheTime = cache || 3600 * 24 * 30;
+    server.on('request', (request: IncomingMessage, response: ServerResponse) => {
+        const { method, url:path } = request;
+        const {pathname} = url.parse(path);
+        if (method !== 'GET') {
+            response.statusCode = 405;
+            response.end('服务器繁忙');
             return;
         }
-        response.end(data);
-    })
-});
+        let fileName = pathname.slice(1);
+        if (fileName === '') {
+            fileName = 'index.html'
+        }
+        fs.readFile(p.resolve(publicPath,  fileName), (err, data) => {
+            const fileType = mime.getType(fileName.split('.')[1]);
+            response.setHeader('Content-Type', fileType);
+            response.setHeader('Cache-Control', cacheTime);
+            if (err) {
+                if (err.errno === -2) {
+                    response.statusCode = 404;
+                    fs.readFile(p.resolve(publicPath, '404.html'), (err, data) => {
+                        response.end(data);
+                    })
+                } else {
+                    response.statusCode = 500;
+                    response.end('服务器正忙');
+                }
+                return;
+            }
+            response.end(data);
+        })
+    });
 
-server.listen(8080, () => {
-    console.log('listening 8080');
-});
+    server.listen(port, () => {
+        console.log(`listening ${port}`);
+    });
+};
 
 
